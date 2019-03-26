9d484838dbb2dd6b3c6a1d0b61b27a98af70d48a
XingEn <xinge@bwit.cc>
2019-03-26 15:01:17 +0800

    实验性提交

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+﻿import Vue from 'vue'
 import App from './App'
 import router from './router'
 
@@ -7,7 +7,7 @@ Vue.config.productionTip = false
 // 引入样式重置
 import '../static/css/reset.css'
 
-// 引入 ElementUI 响应式断点
+// 引入 ElementUI 响应式断点vvv
 import 'element-ui/lib/theme-chalk/display.css';
 
 // 引入及使用 ElementUI
